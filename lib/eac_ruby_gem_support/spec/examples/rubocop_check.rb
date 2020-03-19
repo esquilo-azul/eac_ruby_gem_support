7849dd0ecfa77c36f7beff132f60d0308ee5fc99
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-03-19 20:14:00 -0300

    RSpec.shared_examples('rubocop_check').

---
+++ lib/eac_ruby_gem_support/spec/examples/rubocop_check.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+RSpec.shared_examples 'rubocop_check' do |root_path|
+  before do
+    ::RuboCop::ConfigLoader.ignore_parent_exclusion = true
+  end
+
+  let(:root_path) { root_path }
+  let(:config_store) do
+    r = ::RuboCop::ConfigStore.new
+    r.for(root_path)
+    r
+  end
+  let(:runner) { ::RuboCop::Runner.new({}, config_store) }
+
+  it 'rubocop return ok' do
+    expect(runner.run([])).to eq(true)
+  end
+end
