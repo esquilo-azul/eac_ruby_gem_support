2a32fea23da1b43a7836027cc55bf72badf9c504
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-04-20 22:15:23 -0300

    eac_ruby_gem_support/rspec/example/rubocop_check.rb: move from "eac_ruby_gem_support/spec/examples/rubocop_check.rb".

--- spec/lib/rubocop_check_spec.rb
+++ spec/lib/rubocop_check_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_gem_support/spec/examples/rubocop_check'
+require 'eac_ruby_gem_support/rspec/examples/rubocop_check'
 
 RSpec.describe ::RuboCop do
   include_examples 'rubocop_check', ::File.expand_path('../..', __dir__)
