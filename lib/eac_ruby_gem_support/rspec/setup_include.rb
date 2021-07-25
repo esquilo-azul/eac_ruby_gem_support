ab5ad69f2f927e7e203f1aefbac9c22993a2a26a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-25 12:06:22 -0300

    EacRubyGemSupport::Rspec::SetupInclude.setup_example_persistence.

--- lib/eac_ruby_gem_support/rspec/setup_include.rb
+++ lib/eac_ruby_gem_support/rspec/setup_include.rb
@@ -3,6 +3,7 @@
 require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_gem_support/rspec/helpers/filesystem'
 require 'eac_ruby_gem_support/rspec/specs/rubocop'
+require 'tmpdir'
 
 module EacRubyGemSupport
   class Rspec
@@ -14,6 +15,7 @@ module EacRubyGemSupport
       class << self
         def setup(setup_obj)
           setup_load_path(setup_obj)
+          setup_example_persistence(setup_obj)
           setup_filesystem_helper(setup_obj)
         end
 
@@ -27,6 +29,16 @@ module EacRubyGemSupport
         def setup_load_path(setup_obj)
           $LOAD_PATH.push setup_obj.app_root_path.join('lib').to_path
         end
+
+        def setup_example_persistence(setup_obj)
+          setup_obj.rspec_config.example_status_persistence_file_path =
+            setup_obj.example_persistence_path.to_path
+        end
+      end
+
+      # @return [Pathname]
+      def example_persistence_path
+        ::File.join(::Dir.tmpdir, app_root_path.basename.to_path.variableize).to_pathname
       end
     end
   end
