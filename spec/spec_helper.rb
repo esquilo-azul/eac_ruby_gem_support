6d7a566fec62c12e1af2b1f00468f1d83dd3cc04
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-05-02 08:55:20 -0300

    spec/spec_helper.rb: fix.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,6 +99,5 @@ RSpec.configure do |config|
   #   Kernel.srand config.seed
 end
 
-require 'eac_ruby_utils/rspec/default_setup'
-EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
 require 'eac_ruby_gem_support'
+EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
